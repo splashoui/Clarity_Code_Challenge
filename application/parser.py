6ef881ada83c9a23e1c28d0b05afb3a19d83b01c
Mert <77935876+splashoui@users.noreply.github.com>
2022-06-08 12:07:14 +0200

    Update parser.py

--- application/parser.py
+++ application/parser.py
@@ -1,6 +1,5 @@
 
 # Change this file location depend on your file directory
-
 file_location = 'C:/Users/kurum/Desktop/Clarity_Code_Challenge/' 
 
 class ParserHostConnection(object):
@@ -17,7 +16,6 @@ class ParserHostConnection(object):
 
     If there is not any connections happened in this given period of time to the host_name, it creates error logs 
     under "error_logs" folder as a text file with the file name format : "failed_init_time_end_time_host_name.txt"
-
     
     """
 
@@ -26,25 +24,30 @@ class ParserHostConnection(object):
         self.init_time = init_time
         self.end_time = end_time
         self.host_name = host_name
- 
+        
     def create_list_of_hosts_to_connected(self):
         
         """
-
+        
         Checks the generated connections to the given host name during the given time period. 
         Stores the connections in an empty list.
 
         """
+        
         listed_information_to = []
+        
         # Reads the input file line by line.  
-        with open(self.file_path,'r') as f:
+        with open(self.file_path, 'r') as f:
             text = f.read().splitlines()
+            
             # Checks every line with for loop to find the match with given host_name and given period. 
             for i in range(len(text)):
+                
                 # If the connections are found, stores them in a list.
                 if text[i].split(' ')[2:3] == [f'{self.host_name}'] and self.init_time <= int(text[i].split(' ')[0]) <= self.end_time:
                     listed_information_to.append(text[i])
-        return listed_information_to,self.init_time,self.end_time,self.host_name
+                    
+        return listed_information_to, self.init_time, self.end_time, self.host_name
 
     def create_list_of_hosts_from_connected(self):
 
@@ -57,7 +60,7 @@ class ParserHostConnection(object):
 
         listed_information_from = []
          # Reads the input file line by line.
-        with open(self.file_path,'r') as f:
+        with open(self.file_path, 'r') as f:
             text = f.read().splitlines()
             # Checks every line with for loop to find the match with given host_name and given period. 
             for i in range(len(text)):
@@ -69,18 +72,26 @@ class ParserHostConnection(object):
     def run_store_in_logs(self):
 
         """
-
+        
         Calls the create_list_of_hosts_to_connected method to bring the listed information and store them in a log file.
 
-
         """
+        
         # Takes the output of other function and assign to the new variables.
-        listed_information_to,self.init_time,self.end_time,self.host_name = self.create_list_of_hosts_to_connected()
+        listed_information_to, self.init_time, self.end_time, self.host_name = self.create_list_of_hosts_to_connected()
         
         # In case there are connections, print information and create log file with the file format "init_time_end_time_host_name.txt"
         if listed_information_to:
             print(f'{len(listed_information_to)} hosts were connected to the host "{self.host_name}" during the given period "{self.init_time}" and "{self.end_time}"\n{listed_information_to}')
-            final_path = file_location +'logs/parsed_host_list_files'+'/'+str(self.init_time)+'_'+str(self.end_time)+'_'+self.host_name+'.txt'
+            final_path = file_location +'logs/parsed_host_list_files'
+                                       +'/'
+                                       +str(self.init_time)
+                                       +'_'
+                                       +str(self.end_time)
+                                       +'_'
+                                       +self.host_name
+                                       +'.txt'
+            
             with open(final_path, 'w') as file:
                 for row in listed_information_to:
                     s = "".join(map(str, row))
@@ -92,7 +103,16 @@ class ParserHostConnection(object):
         # with the file format : failed_init_time_end_time_host_name.txt
         else:
             print(f'There is not any host connected to the host "{self.host_name}" yet during the given period "{self.init_time}" and "{self.end_time}"')
-            final_path_error = file_location +'logs/error_logs'+'/'+'failed_'+str(self.init_time)+'_'+str(self.end_time)+'_'+self.host_name+'.txt'
+            final_path_error = file_location 
+                               +'logs/error_logs'
+                               +'/'
+                               +'failed_'
+                               +str(self.init_time)
+                               +'_'
+                               +str(self.end_time)
+                               +'_'
+                               +self.host_name
+                               +'.txt'
             with open(final_path_error, 'w') as file:
                 file.write(f'There is not any host connected to the host "{self.host_name}" yet during the given period "{self.init_time}" and "{self.end_time}"')
             print(f"Error log file has been created under the {file_location+'logs/error_logs'} folder with the name 'failed_{str(self.init_time)}_{str(self.end_time)}_{self.host_name}.txt'")
